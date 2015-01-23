75e7c6e94b63c99b723e24d4b6ae4b1d61e67498
ivorkill <davevanrossum@gmail.com>
2015-01-23 01:13:34 +0100

    Unity Tank Game

---
+++ Assets/Scripts/Movetank.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Movetank : MonoBehaviour {
+	public float rotationspeed = 1.5f;
+	public float moveSpeed = 0.2f;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	if (Input.GetKey (KeyCode.UpArrow)) 
+		{
+			transform.Translate(Vector3.forward * moveSpeed);	
+		}
+	if (Input.GetKey (KeyCode.DownArrow)) 
+		{
+			transform.Translate(-Vector3.forward * moveSpeed);	
+		}
+	if (Input.GetKey (KeyCode.LeftArrow)) 
+		{
+			transform.Rotate(-Vector3.up * rotationspeed);	
+		}
+	if (Input.GetKey (KeyCode.RightArrow)) 
+		{
+			transform.Rotate(Vector3.up * rotationspeed);	
+		}
+	}
+}
---
+++ Assets/Scripts/Shoot.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Shoot : MonoBehaviour {
+
+	public GameObject bulletPre;
+	private GameObject turret;
+	private GameObject nozzle;
+	// Use this for initialization
+	void Start () {
+		Transform[] Transforms = GetComponentsInChildren<Transform> ();
+		foreach (Transform t in Transforms) 
+		{
+			if(t.gameObject.name == "Turret")
+			{
+				turret = t.gameObject;
+			}
+			if (t.gameObject.name == "Nozzle")
+			{
+				nozzle = t.gameObject;
+			}
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetButtonDown ("Fire1")) 
+		{
+			Quaternion rotation = Quaternion.Euler(Vector3.up * turret.transform.rotation.eulerAngles.y);
+
+			Instantiate(bulletPre, nozzle.transform.position, rotation);   
+		}
+	}
+}
